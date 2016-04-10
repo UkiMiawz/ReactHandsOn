f0d23604e5bc7176e9190800449db969c92b3c53
Melisa Cecilia <melisa.cecilia.mail@gmail.com>
2016-04-10 20:14:06 +0200

    Add tutorial 3 - using props

---
+++ server/public/scripts/tutorial3.js
@@ -0,0 +1,48 @@
+var CommentBox = React.createClass({
+  render: function(){
+    return(
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function(){
+    return(
+      <div className="commentList">
+        <Comment author="bububu">This is one comment</Comment>
+        <Comment author="bababa">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function(){
+    return(
+      <div className="commentForm">
+        Hello world I am a comment form
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function(){
+    return(
+      <div className="comment">
+        <h2 class="commentAuthor">{this.props.author}</h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+)
